1f4dc5d72930379248e983c600b31e6a8f2a7d2b
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-03-16 15:02:15 -0300

    Model for Course

---
+++ db/migrate/20090316180046_create_courses.rb
@@ -0,0 +1,19 @@
+class CreateCourses < ActiveRecord::Migration
+  def self.up
+    create_table :courses do |t|
+      t.date :period_from
+      t.date :period_to
+      t.text :objectives
+      t.text :results
+      t.text :teachers_selection_process
+      t.text :students_selection_process
+      t.text :communities
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :courses
+  end
+end
