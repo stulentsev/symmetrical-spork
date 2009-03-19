fbe84c94de479b32dc672008957f62aaf6c5d8e9
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-03-19 15:52:23 -0300

    Add 'destroy' button for activity items

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -66,10 +66,5 @@ class ActivitiesController < ApplicationController
   def destroy
     @activity = Activity.find(params[:id])
     @activity.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(activities_url) }
-      format.xml  { head :ok }
-    end
   end
 end
