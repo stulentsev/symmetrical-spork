b184ef0cf2fd423edeac1a72111cd6092556326a
Marcelo Mustafa Murad <marcelo.murad@dtmconsultoria.com>
2009-03-26 09:48:51 -0300

    Creation of CourseStudentProfile after initialize Course.

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,5 +9,6 @@ class Course < ActiveRecord::Base
   def after_initialize
     self.first_week_methodology ||= FirstWeekMethodology.new()
     self.rotate_methodology ||= RotateMethodology.new()
+    self.student_profile ||=  CourseStudentProfile.new
   end
 end
