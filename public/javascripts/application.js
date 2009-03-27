1096735042d13336b63c28cb215a52cfc74f8ddc
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-03-27 16:32:25 -0300

    Move javascript code to application.js Show elements instantly (instead of fading in)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function initialize_collapsible_headers(css_selector)
+{
+    $$(css_selector).each(function(h2) {
+        h2.onclick = function()
+        {
+            var is_open = this.readAttribute('class') == 'aberto';
+            if (is_open)
+            {
+                this.siblings().each(function(el) {
+                    el.hide();
+                });
+                this.writeAttribute('class', 'fechado');
+            }
+            else
+            {
+                this.siblings().each(function(el) {
+                    el.show();
+                });
+                this.writeAttribute('class', 'aberto');
+            }
+            return false;
+        }
+    });
+}
