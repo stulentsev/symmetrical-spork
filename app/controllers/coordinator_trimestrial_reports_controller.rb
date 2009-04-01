2e228a898dff7ce025c405c4841f77ce0525dfb2
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-04-01 12:56:42 -0300

    Fix bugs with routing

--- app/controllers/coordinator_trimestrial_reports_controller.rb
+++ app/controllers/coordinator_trimestrial_reports_controller.rb
@@ -9,15 +9,13 @@ class CoordinatorTrimestrialReportsController < ApplicationController
             @coordinator_trimestrial_report.update_attributes(params[:coordinator_trimestrial_report])
       @rep_with_deadline.status = 2 # completed
       @rep_with_deadline.save
-      redirect_to edit_course_coordinator_trimestrial_report_url(params[:id])
+      redirect_to edit_course_coordinator_trimestrial_report_url(params[:course_id], params[:id])
       return
     end
 
     respond_to do |format|
       if @coordinator_trimestrial_report.update_attributes(params[:coordinator_trimestrial_report])
         flash[:notice] = 'CoordinatorTrimestrialReport was successfully updated.'
-        format.html { redirect_to(@coordinator_trimestrial_report) }
-        format.xml  { head :ok }
         format.json {render :json => @coordinator_trimestrial_report}
       else
         format.html { render :action => "edit" }
@@ -27,7 +25,7 @@ class CoordinatorTrimestrialReportsController < ApplicationController
   end
 
 private
-  def init_state()
+  def init_state
     @rep_with_deadline = get_rep_with_deadline(params[:id])
     @coordinator_trimestrial_report = CoordinatorTrimestrialReport.find(@rep_with_deadline.actual_report_id)
     @activity = CoordinatorTrimestrialReportActivity.new(:coordinator_trimestrial_report_id => params[:id])
