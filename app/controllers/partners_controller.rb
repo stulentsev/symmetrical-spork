d42eb2b2204a733c58ee1e9b3931fda773f122de
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-04-01 15:07:01 -0300

    Some code cleanup

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,31 +1,4 @@
 class PartnersController < ApplicationController
-  def index
-    @partner = Partner.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @partner }
-    end
-  end
-
-  def show
-    @partner = Partner.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @partner }
-    end
-  end
-
-  def new
-    @partner = Partner.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @partner }
-    end
-  end
-
   def edit
     @partner = Partner.find(params[:id])
   end
--- app/controllers/project_team_members_controller.rb
+++ app/controllers/project_team_members_controller.rb
@@ -1,42 +1,11 @@
 class ProjectTeamMembersController < ApplicationController
   include UsersHelper
   before_filter :require_user, :get_course
-  # GET /project_team_members
-  # GET /project_team_members.xml
-  def index
-    @project_team_members = ProjectTeamMember.find(:all)
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @project_team_members }
-    end
-  end
-
-  # GET /project_team_members/1
-  # GET /project_team_members/1.xml
-  def show
-    @project_team_member = ProjectTeamMember.new
-    @members = Course.find(params[:course_id]).team_members
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @project_team_member }
-    end
-  end
-
-  # GET /project_team_members/new
-  # GET /project_team_members/new.xml
-  def new
-    @project_team_member = ProjectTeamMember.new(course_id => @course)
-  end
-
-  # GET /project_team_members/1/edit
   def edit
     @project_team_member = ProjectTeamMember.find(params[:id])
   end
 
-  # POST /project_team_members
-  # POST /project_team_members.xml
   def create
     @project_team_member = ProjectTeamMember.new(params[:project_team_member])
     @project_team_member.course = @course
@@ -59,15 +28,11 @@ class ProjectTeamMembersController < ApplicationController
     end
   end
 
-  # PUT /project_team_members/1
-  # PUT /project_team_members/1.xml
   def update
     @project_team_member = ProjectTeamMember.find(params[:id])
     @project_team_member.update_attributes(params[:project_team_member])
   end
 
-  # DELETE /project_team_members/1
-  # DELETE /project_team_members/1.xml
   def destroy
     @project_team_member = ProjectTeamMember.find(params[:id])
     @project_team_member.destroy
