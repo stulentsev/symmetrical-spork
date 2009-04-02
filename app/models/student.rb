1516b84cc4c415c521331c700709be4ab19db7ea
Marcelo Mustafa Murad <marcelo.murad@dtmconsultoria.com>
2009-04-02 16:28:53 -0300

    Student profile with lightbox.

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,7 @@ class Student < ActiveRecord::Base
   belongs_to :user
   belongs_to :course
 
-  has_attached_file :photo, :styles => { :thumb => "78x78#" }
+  has_attached_file :photo, :styles => { :thumb => "78x78#", :profile => "117x117#" }
 
   validates_presence_of :name, :email, :language_id
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
