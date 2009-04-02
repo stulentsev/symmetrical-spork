8bff2756f7e5e23b2fd0846651b16395c3b9f213
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-04-02 15:24:03 -0300

    Go to student performance page from educator report

--- app/helpers/educator_reports_helper.rb
+++ app/helpers/educator_reports_helper.rb
@@ -1,5 +1,12 @@
 module EducatorReportsHelper
+  def student_href_func(student)
+    edit_student_performance_url(student,
+                                 :trimester_id => @educator_report.trimester.id,
+                                 :educator_id => @educator.id)
+  end
+
   def student_onclick_func(student)
-    h('alert("' + @educator.name + ' reports: this is ' + student.name + ' on trimester ' + @educator_report.trimester.id.to_s + '"); return false;')
+    return ''
+    #h('alert("' + @educator.name + ' reports: this is ' + student.name + ' on trimester ' + @educator_report.trimester.id.to_s + '"); return false;')
   end
 end
