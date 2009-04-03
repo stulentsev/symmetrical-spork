4162fd42de7ac75e7388ba852d29776b94941a1f
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-04-03 12:34:43 -0300

    Only fill sample data in development mode

--- db/migrate/20090403132430_fill_sample_data.rb
+++ db/migrate/20090403132430_fill_sample_data.rb
@@ -1,63 +1,66 @@
 class FillSampleData < ActiveRecord::Migration
   def self.up
-    # create course
-    course = Course.create(:period_from => Date.today,
-                           :period_to => 18.months.from_now)
+    config = Rails::Configuration.new
+    if config.environment == 'development'
+      # create course
+      course = Course.create(:period_from => Date.today,
+                             :period_to => 18.months.from_now)
 
-    # create trimesters for course
-    6.times do |num|
-      trim = Trimester.create(:course_id => course.id,
-                              :number => num + 1)
-      course.trimesters << trim
-      course.save
-    end
+      # create trimesters for course
+      6.times do |num|
+        trim = Trimester.create(:course_id => course.id,
+                                :number => num + 1)
+        course.trimesters << trim
+        course.save
+      end
 
-    # create coordinators
-    sergei = User.create( :user_type_id => 1, # coordinator
-                          :login => 'sergei',
-                          :email => 'sergei.tulentsev@gmail.com',
-                          :password => 'sergei',
-                          :password_confirmation => 'sergei')
-    murad = User.create(:user_type_id => 1, # coordinator
-                        :login => 'murad',
-                        :email => 'marcelo.murad@gmail.com',
-                        :password => 'murad123',
-                        :password_confirmation => 'murad123')
-    sergei.setup_reports_for_course(course)
-    murad.setup_reports_for_course(course)
+      # create coordinators
+      sergei = User.create( :user_type_id => 1, # coordinator
+                            :login => 'sergei',
+                            :email => 'sergei.tulentsev@gmail.com',
+                            :password => 'sergei',
+                            :password_confirmation => 'sergei')
+      murad = User.create(:user_type_id => 1, # coordinator
+                          :login => 'murad',
+                          :email => 'marcelo.murad@gmail.com',
+                          :password => 'murad123',
+                          :password_confirmation => 'murad123')
+      sergei.setup_reports_for_course(course)
+      murad.setup_reports_for_course(course)
 
-    # create educadores
-    4.times {|num| create_language_educator(num + 1, course)}
+      # create educadores
+      4.times {|num| create_language_educator(num + 1, course)}
 
-    transversal_user = User.create(:user_type_id => 3, # educador transversal
-                        :login => 'transversal',
-                        :email => 'transversal@mylo.com',
-                        :password => 'transversal',
-                        :password_confirmation => 'transversal')
-    transversal = ProjectTeamMember.create(:course_id => course.id,
-                                        :user_id => transversal_user.id,
-                                        :language_id => 1, # to pass validation
-                                        :name => "Andrew Romashkin (educador transversal para teste)",
-                                        :email => 'transversal@mylo.com')
-    transversal_user.setup_reports_for_course(course)
+      transversal_user = User.create(:user_type_id => 3, # educador transversal
+                          :login => 'transversal',
+                          :email => 'transversal@mylo.com',
+                          :password => 'transversal',
+                          :password_confirmation => 'transversal')
+      transversal = ProjectTeamMember.create(:course_id => course.id,
+                                          :user_id => transversal_user.id,
+                                          :language_id => 1, # to pass validation
+                                          :name => "Andrew Romashkin (educador transversal para teste)",
+                                          :email => 'transversal@mylo.com')
+      transversal_user.setup_reports_for_course(course)
 
-    # create students
-    50.times do |number|
-      student_user = User.create( :user_type_id => 4, # educando
-                                  :login => "student#{number}",
-                                  :email => "student#{number}@mylo.com",
-                                  :password => "student#{number}",
-                                  :password_confirmation => "student#{number}")
-      student = Student.create(:name => "Learnable organism \##{number}",
-                               :email => "student#{number}@mylo.com",
-                               :language_id => 1 + rand(Language.count),
-                               :schooling_id => 1 + rand(Schooling.count),
-                               :user_id => student_user.id,
-                               :course_id => course.id,
-                               :birthday => Date.civil(1982, 1 + rand(12), 1 + rand(28))
-                              )
-      #student_user.setup_reports_for_course(course)
+      # create students
+      50.times do |number|
+        student_user = User.create( :user_type_id => 4, # educando
+                                    :login => "student#{number}",
+                                    :email => "student#{number}@mylo.com",
+                                    :password => "student#{number}",
+                                    :password_confirmation => "student#{number}")
+        student = Student.create(:name => "Learnable organism \##{number}",
+                                 :email => "student#{number}@mylo.com",
+                                 :language_id => 1 + rand(Language.count),
+                                 :schooling_id => 1 + rand(Schooling.count),
+                                 :user_id => student_user.id,
+                                 :course_id => course.id,
+                                 :birthday => Date.civil(1982, 1 + rand(12), 1 + rand(28))
+                                )
+        #student_user.setup_reports_for_course(course)
 
+      end
       # Open all reports
       ReportsWithDeadline.connection.execute("UPDATE reports_with_deadlines SET status = 1")
     end
