6720d275b4479c008322b62bffa6acbe260a769a
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-04-17 17:52:25 -0300

    View search results form, first draft.

--- app/controllers/gestor_controller.rb
+++ app/controllers/gestor_controller.rb
@@ -4,6 +4,30 @@ class GestorController < ApplicationController
 
   def view_reports
 
+    if request.post?
+      temp_course = Course.new(params[:period])
+      results = {}
+      School.find(:all).each do |s|
+        results[s.name] = []
+        if (params[:balch]["school_#{s.id}"] == '1')
+          conditions = ["deadline > ? and deadline < ?",
+                        temp_course.period_from,
+                        temp_course.period_to]
+          if params[:include_current_period] == '1'
+            conditions[0] << " or deadline > ? and deadline < ?"
+            conditions << s.current_course.period_from
+            conditions << s.current_course.period_to
+          end
+          ReportsWithDeadline.find(:all,
+                                    :conditions => conditions).
+                              select{|rwd| params[:selected_reports].member?(rwd.report_id.to_s)}.
+                              each{|rwd| results[s.name] << rwd}
+        end
+      end
+      render :partial => 'gestor/report_search_results',
+             :object => results
+
+    end
   end
 
   def create_reports
