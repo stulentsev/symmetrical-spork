b786bc241f7b367baf446d6a2f8f3ff616d2437a
Sergei Tulentsev <sergei.tulentsev@gmail.com>
2009-04-20 16:23:44 -0300

    Gestor, monitor students: Basic form (with url to regular profile)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :gestor,
-                :member => [:view_reports, :report_search_results, :get_reports_for_user]
+                :member => [:view_reports,
+			    :report_search_results,
+			    :get_reports_for_user,
+			    :monitor_students]
 
   map.resources :deadlines,
                 :member => [:get_reports_for_user]
@@ -107,4 +110,3 @@ ActionController::Routing::Routes.draw do |map|
   #map.connect ':controller/:action/:id'
   #map.connect ':controller/:action/:id.:format'
 end
-
